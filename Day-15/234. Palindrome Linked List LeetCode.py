111909dce664adfddd4074ab10d27a55e7ff0d49
NilayKantharia <125391394+NilayKantharia@users.noreply.github.com>
2024-03-22 23:46:44 +0530

    Create 234. Palindrome Linked List LeetCode.py

---
+++ Day-15/234. Palindrome Linked List LeetCode.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        rev = None
+        fast = head
+        slow = head
+        while fast and fast.next: 
+            fast = fast.next.next
+            rev,rev.next,slow = slow,rev,slow.next
+        if fast:
+            slow = slow.next
+
+        while slow:
+            if slow.val != rev.val:
+                return False
+            else:
+                slow = slow.next
+                rev = rev.next
+        return True
