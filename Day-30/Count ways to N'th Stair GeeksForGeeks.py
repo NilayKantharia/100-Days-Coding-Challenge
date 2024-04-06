dfc0012d8d2f65f8dd926899f919c1c2d38e0f0b
NilayKantharia <125391394+NilayKantharia@users.noreply.github.com>
2024-04-06 23:58:47 +0530

    Create Count ways to N'th Stair GeeksForGeeks.py

---
+++ Day-30/Count ways to N'th Stair GeeksForGeeks.py
@@ -0,0 +1,14 @@
+class Solution:
+
+    #Function to count number of ways to reach the nth stair
+    #when order does not matter.
+    def countWays(self, n):
+
+        res = [0]*(n+1)
+        res[0] = 1
+        res[1] = 1
+        for i in range(2,n+1):
+            res[i] = res[i-2] + 1
+        return res[n]
+        
+
